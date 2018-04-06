2374da94655c851a981f40b10200c0a07c1f5f6f
Chris Cardle <cdcardle@gmail.com>
2018-04-06 15:22:49 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,8 +14,7 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/recipes' do
-    raise params.inspect
-    # @recipe = Recipe.new(:name => params[:name], :ingredients => params[:ingredients], :cook_time => params[:cook_time])
+    @recipe = Recipe.new(:name => params[:name], :ingredients => params[:ingredients], :cook_time => params[:cook_time])
     redirect to '/recipes/#{@recipe.id}'
   end
 
